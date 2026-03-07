288c3d5e4903dbf6a0469d199dd208304d78e029
omar-ys <omaryousef2y@gmail.com>
2026-03-08 02:07:18 +0300

    Add Header component and styles
    - Created Header.jsx component to display the restaurant title and subtitle.
    - Added header.css for styling the header with background image and text formatting.

---
+++ Restaurant/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import '../styles/header.css'
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="header__content">
+                <h1 className="header__title">Jimmy’s Diner</h1>
+                <p className="header__subtitle">The best burgers and pizzas in town.</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
